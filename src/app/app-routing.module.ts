c827757beff84f4d39eaf2610821ca3abf277f1f
hugo <hugo.herrador@gmail.com>
2020-05-25 13:11:18 +0200

    Add logo to header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,25 @@ import { LoginComponent } from './components/shared/login/login.component';
 import { SignupComponent } from './components/shared/signup/signup.component';
 import { HomeComponent } from './components/shared/home/home.component';
 import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+import { PopupSmComponent } from './components/madrid/popup-sm/popup-sm.component';
+import { PopupBigComponent } from './components/madrid/popup-big/popup-big.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'home', component: HomeComponent},
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        children: [
+          { path: 'popup-sm', component: PopupSmComponent },
+          { path: 'popup-big', component: PopupBigComponent }
+        ]
+      }
+    ]
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ]
