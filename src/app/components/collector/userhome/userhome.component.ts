b6f9e23e89340e1065aad3bbb53adbe3983ae2f1
hugo <hugo.herrador@gmail.com>
2020-06-01 21:08:46 +0200

    Storaging user in localStorage not properly yet

--- src/app/components/collector/userhome/userhome.component.ts
+++ src/app/components/collector/userhome/userhome.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { StorageService,LOCAL_STORAGE } from 'ngx-webstorage-service';
 
 @Component({
   selector: 'app-userhome',
@@ -8,9 +9,17 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class UserhomeComponent implements OnInit {
 
-  constructor() { }
+  userName: any;
+
+  constructor(
+    @Inject(LOCAL_STORAGE)
+    private storage: StorageService,
+  ) { }
 
   ngOnInit(): void {
+    this.userName = this.storage.get('user');
+    //const res = JSON.parse(this.storage.get('user'))
+    //this.userName = res.username;
   }
 
 }
