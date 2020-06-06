a056339902c87d2a66fd23a5a39fd9269589c9aa
hugo <hugo.herrador@gmail.com>
2020-06-06 13:25:07 +0200

    Add ngIf

--- src/app/components/shared/popup/popup.component.ts
+++ src/app/components/shared/popup/popup.component.ts
@@ -20,10 +20,32 @@ export class PopupComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+
+    this.data = {
+      comun: [],
+      air: [],
+      meteo: []
+    }
+
     if (this.mapboxservice.meteoStationClicked === undefined) {
-      debugger
       this.stationsService.getAirStationById(this.mapboxservice.airStationClicked).subscribe((res) => {
-        this.data = res;
+        let estacion = res[0].estacion;
+        let dir = res[0].direccion;
+        let alt = res[0].altitud;
+        let long = res[0].lon_geogra;
+        let lat = res[0].lat_geogra;
+        this.data.comun.push(estacion, dir, alt, long, lat);
+        let no2 = res[0].no2;
+        let so2 = res[0].so2;
+        let co = res[0].co;
+        let pm10 = res[0].pm10;
+        let pm25 = res[0].pm2_5;
+        let o3 = res[0].o3;
+        let btx = res[0].btx;
+        let hc = res[0].hc;
+        debugger
+        this.data.air.push(no2, so2, co, pm10, pm25, o3, btx, hc);
+        debugger
         console.log('soy una res con info de Air :' + res[0])
       }, (error) => {
         console.log(error)
@@ -32,14 +54,25 @@ export class PopupComponent implements OnInit {
       debugger
       this.stationsService.getMeteoStationById(this.mapboxservice.meteoStationClicked).subscribe((res) => {
         debugger
-        this.data = res;
+        let estacion = res[0].estacion;
+        let dir = res[0].direccion;
+        let alt = res[0].altitud;
+        let long = res[0].lon_geogra;
+        let lat = res[0].lat_geogra;
+        this.data.comun.push(estacion, dir, alt, long, lat);
+        let viento = res[0].v_viento;
+        let dirviento = res[0].dir_viento;
+        let temp = res[0].temperatura;
+        let hum = res[0].hum_rel;
+        let presion = res[0].presion;
+        let radiac = res[0].rad_solar;
+        let precipit = res[0].precipitacion;
+        debugger
+        this.data.meteo.push(viento, dirviento, temp, hum, presion, radiac, precipit);
+        debugger
         console.log('soy una res con info de Meteo :' + res[0])
       })
     }
-  }
-
-  loadData() {
-
   }
 
   close() {
